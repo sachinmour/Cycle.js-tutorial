2941c72502fd7e38b66b18fd5f1ddcfec1df18ec
Sachin Mour <rtr.sachinmour@gmail.com>
2018-01-24 10:22:34 +0100

    working directory

---
+++ 1/script.js
@@ -0,0 +1,11 @@
+// Logic
+xs.periodic(1000)                  // ---!---!---!---!---
+.fold(prev => prev + 1, 0)         // 0--1---2---3---4---
+.map(i => `Seconds elapsed: ${i}`)
+// Effects
+.subscribe({
+    next: str => {
+        const elem = document.querySelector('#app');
+        elem.textContent = str;
+    }
+});
